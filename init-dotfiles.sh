8b5e55b35c25b72e2ec15b67ffc48cf7e0702c00
takiyu <takiyu1025.txu.development@gmail.com>
2016-04-21 02:18:02 +0900

    Add mplayer config

--- init-dotfiles.sh
+++ init-dotfiles.sh
@@ -3,8 +3,8 @@
 # TARGETS=.?*
 TARGETS=(.Xmodmap .bash_profile .bashrc .jshintrc .latexmkrc .pep8 \
          .xinitrc .xmobarrc .xmonad \
-         .config/zathura/zathurarc)
-TARGET_DIRS=(.config/zathura/)
+         .config/zathura/zathurarc .mplayer/config)
+TARGET_DIRS=(.config/zathura/ .mplayer/)
 
 
 # utility functions
@@ -45,13 +45,14 @@ done
 for target in ${TARGETS[@]}; do
     pwd_target="$PWD/$target"
     home_target="$HOME/$target"
-    home_backup="$HOME/_$target"
+    home_backup="$HOME/$target"_
     if [ ! -e $pwd_target ]; then
         echo "[Error] not exist $pwd_target"
     elif [ -e $home_target ]; then
         echo -n "[Ask] $home_target is already exist. Move? [y/n] "
         ret=`yn_prompt`
         if [ $ret -eq 0 ]; then
+            echo "mv $home_target $home_backup"
             mv $home_target $home_backup
             create_link $pwd_target $home_target
         fi
