b766a4bca3d8e546e8553e4552b428d9c872d893
takiyu <takiyu1025.txu.development@gmail.com>
2018-08-03 09:14:56 +0900

    Add opencv building script with contrib for archlinux

---
+++ builders/opencv/archlinux/install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+version='3.4'
+work_dir=$PWD
+
+cd $PWD
+if [ ! -f opencv ]; then
+    git clone https://github.com/opencv/opencv
+    cd opencv
+    git checkout $version
+fi
+opencv_dir=$PWD/opencv
+
+cd $PWD
+if [ ! -f opencv_contrib ]; then
+    git clone https://github.com/opencv/opencv_contrib
+    cd opencv_contrib
+    git checkout $version
+fi
+contrib_dir=$PWD/opencv_contrib
+
+cd $opencv_dir
+mkdir build
+cd build
+cmake -DBUILD_DOCS=ON -DBUILD_EXAMPLES=ON -DCMAKE_BUILD_TYPE=RELEASE -DWITH_TBB=ON -DWITH_GTK=ON -DWITH_OPENGL=ON -DWITH_QT=ON -DINSTALL_C_EXAMPLES=ON -DWITH_OPENCL=OFF -DWITH_CUDA=OFF -DWITH_OPENNI=OFF -DWITH_UNICAP=ON -DWITH_V4L=ON -DWITH_XINE=ON -DOPENCV_EXTRA_MODULES_PATH=$contrib_dir/modules ..
+make -j4
+sudo make install
+sudo ldconfig
