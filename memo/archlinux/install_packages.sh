ce436bf3d4d15c0db49757c3732b23237c239a59
takiyu <takiyu1025.txu.development@gmail.com>
2020-10-29 01:52:15 +0900

    Update packages

--- memo/archlinux/install_packages.sh
+++ memo/archlinux/install_packages.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Common software
+trizen -S bash_completion
 trizen -S neovim neovim-qt
 trizen -S linux-headers
 trizen -S pkgfile
 trizen -S net-tools
 trizen -S words
 trizen -S w3m
-trizen -S nkf lv
 trizen -S htop
 trizen -S alsa-firmware pavucontrol
 trizen -S zathura zathura-pdf-poppler poppler-data
@@ -16,24 +16,25 @@ trizen -S smplayer mcomix
 trizen -S firefox
 trizen -S gnome-calculator
 trizen -S slack-desktop
-trizen -S goldendict
+# trizen -S goldendict
 trizen -S simplescreenrecorder
 trizen -S chromium chromium-widevine
 trizen -S pepper-flash
 trizen -S cmigemo-git
-trizen -S unzip-iconv
-trizen -S virtualbox
-trizen -S virtualbox-ext-oracle
-trizen -S gimp inkscape
-trizen -S sane xsane simple-scan
+# trizen -S virtualbox
+# trizen -S virtualbox-ext-oracle
+# trizen -S sane xsane simple-scan
+# trizen -S gimp inkscape blender
 
 # Development
 trizen -S highlight
+trizen -S cmake
 trizen -S clang ctags global
 trizen -S jdk8-openjdk jdtls
 sudo archlinux-java set java-8-openjdk
 trizen -S python python-pip python2 python2-pip python-language-server
 sudo pip install pyflakes pep8
+sudo pip install neovim
 trizen -S nodejs npm
 sudo npm install -g jsxhint
 trizen -S texlive-core texlive-langjapanese texlive-latexextra texlive-most
@@ -42,7 +43,7 @@ trizen -S opencv
 trizen -S paraview
 # trizen -S openni2-libfreenect libfeenect2
 # trizen -S pcl
-trizen -S cmake premake4 premake-git
+# trizen -S premake4 premake-git
 # trizen -S ocl-icd intel-opencl-runtime opencl-headers
 
 # Fonts
@@ -54,6 +55,7 @@ trizen -S ttf-liberation
 trizen -S noto-fonts-emoji
 
 # Desktop
+trizen -S xorg
 trizen -S xmonad xmonad-contrib xmobar
 trizen -S xfce4 xfce4-goodies xfce4-notifyd xfce4-screenshooter xfce4-taskmanager
 trizen -S fcitx fcitx-mozc fcitx-configtool fcitx-gtk2 fcitx-gtk3 fcitx-qt4 fcitx-qt5
@@ -64,12 +66,12 @@ trizen -S gvfs gvfs-smb sshfs gvfs-mtp mtpfs
 trizen -S xclip
 
 # Android Development
-trizen -S android-sdk android-sdk-platform-tools android-sdk-build-tools android-sdk-cmake
-trizen -S android-platform android-ndk
-trizen -S pidcat
-trizen -S android-studio
+# trizen -S android-sdk android-sdk-platform-tools android-sdk-build-tools android-sdk-cmake
+# trizen -S android-platform android-ndk
+# trizen -S pidcat
+# trizen -S android-studio
 
 # Bluetooth
-trizen -S xfce4-pulseaudio-plugin
-trizen -S pulseaudio-bluetooth
-trizen -S blueman
+# trizen -S xfce4-pulseaudio-plugin
+# trizen -S pulseaudio-bluetooth
+# trizen -S blueman
