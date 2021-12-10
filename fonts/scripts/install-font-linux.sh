57ecd9d747e49027f45d26b8a6a6ed480dd67f7e
takiyu <8927943+takiyu@users.noreply.github.com>
2021-12-10 10:11:05 +0900

    Update install-font-linux.sh

--- fonts/scripts/install-font-linux.sh
+++ fonts/scripts/install-font-linux.sh
@@ -8,8 +8,8 @@ FONT_DIRNAME=~/.local/share/fonts/
 mkdir -p $FONT_DIRNAME
 
 # Copy
-cp dotfiles/fonts/*.ttc $FONT_DIRNAME
-cp dotfiles/fonts/*.ttf $FONT_DIRNAME
+cp $dotfiles/fonts/*.ttc $FONT_DIRNAME
+cp $dotfiles/fonts/*.ttf $FONT_DIRNAME
 
 # Update cache
 fc-cache -f -v
