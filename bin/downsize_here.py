20944a41ff2401c3fe118a6e53fbf1c37719839a
takiyu <takiyu1025.txu.development@gmail.com>
2022-08-21 07:28:31 +0000

    Fix downsize script

--- bin/downsize_here.py
+++ bin/downsize_here.py
@@ -4,10 +4,43 @@ import shutil
 import glob
 import os
 import os.path as osp
+from multiprocessing.pool import Pool
+
+
+def process_one(src_filename):
+    print(f'Process: {src_filename}')
+    if not osp.isfile(src_filename):
+        return
+
+    # Create result filename
+    base_dirname = osp.basename(src_dirname)
+    base_filename = osp.basename(src_filename)
+    basename = f'{base_dirname}  {base_filename}'
+    dst_filename = osp.join(RESULT_DIRNAME, basename)
+    if osp.exists(dst_filename):
+        print(f' > Skip existing file: {dst_filename}')
+        return
+
+    # Downsize process
+    _, ext = osp.splitext(dst_filename)
+    ext = ext.lower()
+    if ext in ['.jpg', '*.jepg', 'png']:
+        # Copy (with meta data)
+        shutil.copy2(src_filename, dst_filename)
+        # Downsize image
+        subprocess.run(['downsize_image.sh',
+                        f'{osp.abspath(dst_filename)}'])
+    elif ext in ['.mp4']:
+        # Downsize video
+        subprocess.run(['downsize_video.sh',
+                        src_filename, dst_filename])
+    else:
+        print(f'Unsupported file extension: {ext}')
+
 
 if __name__ == '__main__':
     # Create result directory
-    RESULT_DIRNAME = './downsized_result'
+    RESULT_DIRNAME = './Downsized'
     os.makedirs(RESULT_DIRNAME, exist_ok=True)
 
     # Collect sub-directories
@@ -16,33 +49,12 @@ if __name__ == '__main__':
         if not osp.isdir(src_dirname):
             continue
 
+        # Skip result directory
+        if osp.basename(src_dirname) == osp.basename(RESULT_DIRNAME):
+            continue
+
         # Collect files
         src_filenames = glob.glob(osp.join(src_dirname, '*'))
-        for src_filename in src_filenames:
-            print(f'Process: {src_filename}')
-            if not osp.isfile(src_filename):
-                continue
-
-            # Create result filename
-            base_dirname = osp.basename(src_dirname)
-            base_filename = osp.basename(src_filename)
-            basename = f'{base_dirname}  {base_filename}'
-            dst_filename = osp.join(RESULT_DIRNAME, basename)
-            if osp.exists(dst_filename):
-                print(f'Skip existing file: {dst_filename}')
-
-            # Downsize process
-            _, ext = osp.splitext(dst_filename)
-            ext = ext.lower()
-            if ext in ['.jpg', '*.jepg', 'png']:
-                # Copy (with meta data)
-                shutil.copy2(src_filename, dst_filename)
-                # Downsize image
-                subprocess.run(['downsize_image.sh',
-                                f'{osp.abspath(dst_filename)}'])
-            elif ext in ['.mp4']:
-                # Downsize video
-                subprocess.run(['downsize_video.sh',
-                                src_filename, dst_filename])
-            else:
-                print(f'Unsupported file extension: {ext}')
+        # Process in parallel
+        pool = Pool()
+        pool.map(process_one, src_filenames)
