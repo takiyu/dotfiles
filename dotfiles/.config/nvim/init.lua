9c8eb2e49d85bd95ff54e6823ddf1e38d6b8d676
takiyu <takiyu1025.txu.development@gmail.com>
2023-07-02 13:53:02 +0000

    Add first script

---
+++ dotfiles/.config/nvim/init.lua
@@ -0,0 +1,20 @@
+-- =============================================================================
+--                                      init.lua
+-- =============================================================================
+print(vim.fn.stdpath('data'))
+
+-- Install Jetpack
+local jetpackfile = vim.fn.stdpath('data') ..'/site/pack/jetpack/opt/vim-jetpack/plugin/jetpack.vim'
+local jetpackurl = 'https://raw.githubusercontent.com/tani/vim-jetpack/master/plugin/jetpack.vim'
+if vim.fn.filereadable(jetpackfile) == 0 then
+  vim.fn.system(string.format('curl -fsSLo %s --create-dirs %s', jetpackfile, jetpackurl))
+end
+
+-- Install plugins
+local jetpack = require('jetpack')
+for _, name in ipairs(jetpack.names()) do
+  if not jetpack.tap(name) then
+    jetpack.sync()
+    break
+  end
+end
