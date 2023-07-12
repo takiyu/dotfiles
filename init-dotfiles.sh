7e361e0d86e6e92afb289a76146582f62f6ed7ab
takiyu <takiyu1025.txu.development@gmail.com>
2023-07-12 13:32:59 +0000

    Add xbindkeysrc for mouse extra keys

--- init-dotfiles.sh
+++ init-dotfiles.sh
@@ -88,7 +88,7 @@ if [ $platform == 'Linux' ]; then
              .clang-tidy .jshintrc .pep8 .tmux.conf .config/zathura/zathurarc \
              .mplayer/config .config/matplotlib/matplotlibrc \
              .vimrc .config/nvim/init.vim .config/nvim/ginit.vim \
-             .config/nvim/dein .config/pycodestyle .lesskey .vsnip)
+             .config/nvim/dein .config/pycodestyle .lesskey .vsnip .xbindkeysrc)
     TARGET_DIRS=(.config/zathura/ .mplayer/ .config/matplotlib/ .config/nvim/)
 
     # First, create directories
