1676b9c7b9519e986d28bca570b3e93ff9a76533
takiyu <takiyu1025.txu.development@gmail.com>
2023-09-20 12:08:22 +0000

    Add lang.lua

--- dotfiles/.config/nvim/lua/plugins/vimhelpgenerator.lua
+++
@@ -1,9 +0,0 @@
-local vimscript_code = [[
-" ==============================================================================
-"                                  vimhelpgenerator
-" ==============================================================================
-let g:vimhelpgenerator_defaultlanguage = 'en'
-let g:vimhelpgenerator_author = 'takiyu'
-]]
-
-return {}
