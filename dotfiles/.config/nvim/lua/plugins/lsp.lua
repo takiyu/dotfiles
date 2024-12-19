564e0cf72c2649719b7d5eb4af60993b13f91453
takiyu <takiyu1025.txu.development@gmail.com>
2024-12-20 00:32:27 +0900

    Set diagnostic borders

--- dotfiles/.config/nvim/lua/plugins/lsp.lua
+++ dotfiles/.config/nvim/lua/plugins/lsp.lua
@@ -12,6 +12,18 @@ return {
           signs = true,
           underline = true,
           update_in_insert = false,
+          border = 'rounded'  -- Border
+        }
+      )
+      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+        vim.lsp.handlers.hover, {
+          border = 'rounded'  -- Border
+        }
+        }
+      )
+      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+        vim.lsp.handlers.signature_help, {
+          border = 'rounded'  -- Border
         }
       )
       -- Diagnostic gutter
