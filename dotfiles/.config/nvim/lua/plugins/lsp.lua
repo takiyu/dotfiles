a7a677936da7845f1b65a546380abb78934c1291
takiyu <takiyu1025.txu.development@gmail.com>
2026-04-30 15:59:29 +0900

    Refactor: Remove custom diagnostic gutter signs

--- dotfiles/.config/nvim/lua/plugins/lsp.lua
+++ dotfiles/.config/nvim/lua/plugins/lsp.lua
@@ -12,13 +12,6 @@ return {
         opts.border = opts.border or 'rounded'
         return org_func(contents, syntax, opts, ...)
       end
-
-      -- Diagnostic gutter
-      local signs = { Error = "🔥", Warn = "🚧", Hint = "🐬", Info = "🖊️" }
-      for type, icon in pairs(signs) do
-        local hl = "DiagnosticSign" .. type
-        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-      end
     end,
     config = function()
       -- Key mappings
