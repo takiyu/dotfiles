d0da8b1d00b83e747c57c98f96ff2e3e5b1db01f
Tester <test@test.com>
2026-05-22 14:31:30 +0900

    Fix: Switch cmp-calc plugin to custom fork

--- dotfiles/.config/nvim/lua/plugins/lsp_completion.lua
+++ dotfiles/.config/nvim/lua/plugins/lsp_completion.lua
@@ -9,7 +9,7 @@ return {
   { 'hrsh7th/cmp-path' },
   { 'hrsh7th/cmp-cmdline' },
   { 'hrsh7th/cmp-emoji' },
-  { 'hrsh7th/cmp-calc' },
+  { 'takiyu/cmp-calc' },  -- Self custom
   { 'octaltree/cmp-look' },
 
   ------------------------------------------------------------------------------
